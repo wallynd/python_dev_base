e18b54a88feaa56356088d4368e0799c443a2dd9
Walter Tuholski <walter_tuholski@mbp-wtuholski.local>
2017-03-05 21:24:51 -0800

    adding a class

--- src/foo.py
+++ src/foo.py
@@ -1,9 +1,24 @@
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-print 'Hola Mundo!'
-psdfsdfrint 'Hola Mundo!'
+
+
+class Foo(object): 
+
+    def __init__(self):
+
+        object.__init__(self)
+        print 'Constructing Foo'
+        self.foo_bar = 0
+        self.__bar(42)
+      
+    def getFooBar(self):
+        print 'Get the Foo Bar'
+        return self.foo_bar
+
+    def __bar(self, num):
+
+        print 'Private Bar Function'
+        self.foo_bar = num
+
+
+if __name__ == "__main__":
+    fooBar = Foo()
+    print str(fooBar.getFooBar())
